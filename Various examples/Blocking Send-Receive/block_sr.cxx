c6b1072370085437257e4dccf3096dbe2280b096
Michael Løiten <michael.l.magnussen@gmail.com>
2014-06-05 17:07:52 +0200

    Did some more programs, are they updated?

--- Various examples/Blocking Send-Receive/block_sr.cxx
+++ Various examples/Blocking Send-Receive/block_sr.cxx
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <math.h>
+#include <iostream> 
 
 int main (int argc, char **argv) {
     int myrank,i;
@@ -15,6 +16,8 @@ int main (int argc, char **argv) {
     // Get the rank
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
+    std::cout << "Hello, my rank is: " << myrank << "\n" << std::endl;
+
     if( myrank == 0 )        /* Send a message */
     {
         for (i=0;i<100;++i)
@@ -26,11 +29,15 @@ int main (int argc, char **argv) {
         // Tag it with the number 17
         // The communicator is 17
         MPI_Send( a, 100, MPI_DOUBLE, 1, 17, MPI_COMM_WORLD );
+        std::cout << "Rank " << myrank << " just sent a to rank 1" << std::endl;
     }
     else if( myrank == 1 )
+    {
         // Receive a from 0, but store it as b...
-        MPI_Recv( b, 100, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD, &status );   
-  
+        MPI_Recv( b, 100, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD, &status );
+        std::cout << "Rank " << myrank << " just received a message from rank 0"
+                  << std::endl;
+    }
     // Terminate MPI
     MPI_Finalize();
     
